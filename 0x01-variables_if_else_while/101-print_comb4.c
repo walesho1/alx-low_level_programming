386a788c383338b9e31d40873a0b8e20940572c9
olawale <waleshogbaike@yahoo.com>
2022-09-11 10:16:27 -0700

    commit

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -7,30 +7,29 @@
  */
 int main(void)
 {
+
 	int n, m, l;
 
 	for (n = 48; n < 58; n++)
 	{
-		putchar(n);
+		for (m = 49; m < 58; m++)
+		{
+			for (l = 50; l < 58; l++)
+			{
+				if (l > m && m > n)
+				{
+					putchar(n);
+					putchar(m);
+					putchar(l);
+					if (n != 55 || m != 56)
+r(',');
+					putchar(' ');
+				}
+			}
+		}
 	}
-	for (m = 49; m < 58; m++)
-	{
-		putchar(m);
-	}
-	for (l = 50; l < 58; l++)
-	{
-		putchar(l);
-	}
-	if (l > m && m > n)
-	{
-		putchar(',');
-	}
-	if (n != 55 || m != 56)
-	{
-		putchar(' ');
-	}
+}
+putchar('\n');
 
-	putchar('\n');
-
-	return (0);
+return (0);
 }
