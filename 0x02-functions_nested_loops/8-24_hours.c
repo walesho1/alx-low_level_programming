910433357dcb81bdfe36fc130ea623d89fd102e5
olawale <waleshogbaike@yahoo.com>
2022-09-13 18:58:30 -0700

    commit

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -12,19 +12,20 @@ void jack_bauer(void)
 
 	while (hours <= 23)
 	{
-		while (minutes <= 59);
-		{
-			mins_remainder - minutes % 10;
-			hours_remainder - hours % 10;
-			_putcher(hours / 10 + '0');
-			_putchar(hours_remainder + '0')
-				_putchar(':');
-			_putchar(minutes / 10 + '0');
-			_putchar(min_remainder + '0');
-			minutes++;
-			_putchar('\n');
-		}
-		hours++;
-		minutes = 0;
+	while (minutes <= 59);
+	{
+
+		mins_remainder - minutes % 10;
+		hours_remainder - hours % 10;
+		_putcher(hours / 10 + '0');
+		_putchar(hours_remainder + '0')
+			_putchar(':');
+		_putchar(minutes / 10 + '0');
+		_putchar(min_remainder + '0');
+		minutes++;
+		_putchar('\n');
+	}
+	hours++;
+	minutes = 0;
 	}
 }
