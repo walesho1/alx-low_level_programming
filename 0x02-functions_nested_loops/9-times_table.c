4a02d3c7289e446f6a0a2abcb4a6d98a526aaf24
olawale <waleshogbaike@yahoo.com>
2022-09-14 07:52:44 -0700

    commit

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,36 +5,38 @@
  *
  * Return: void
  */
-
 void times_table(void)
 {
+
 	int row, column, product, tens, ones;
 
 	for (row = 0; row <= 9; row++)
 	{
-		product = row * column;
-		tens = product / 10;
-		ones = product % 10;
+		for (column = 0; column <= 9; column++)
+		{
+			product = row * column;
+			tens = product / 10;
+			ones = products % 10;
 
-
-		if (column == 0)
-		{
-			_putchar('0');
+			if (column == 0)
+			{
+				_putchar('0');
+			}
+			else if (product < 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(ones + '0');
+			}
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(tens + '0');
+				_putchar(ones + '0');
+			}
 		}
-		else if (product < 10)
-		{
-			_putchar(',');
-			_putchar(' ');
-			_putchar(' ');
-			_putchar(ones + '0');
-		}
-		else
-		{
-			_putchar(',');
-			_putchar(' ');
-			_putchar(tens + '0');
-			_putchar(ones + '0');
-		}
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
