d3b14a99abd36f3490cbeefc30e7f5b5c6e504bd
olawale <waleshogbaike@yahoo.com>
2022-09-14 03:33:49 -0700

    commit

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -7,6 +7,7 @@
 void print_times_table(int n)
 {
 	int i, j, k;
+
 	if (n >= 0 && n <= 15)
 	{
 		for (i = 0; i <= n; i++)
@@ -40,7 +41,7 @@ void print_times_table(int n)
 					_putchar(' ');
 					_putchar((k / 100) + '0');
 					_putchar(((k / 10) % 10) + '0');
-					_putchar((k % 10) + '0');
+					_putchar((k % 10) + 10) + '0');
 				}
 			}
 			_putchar('\n');
