d9ba4a962dbf6c1009e52633eb622ed07647f630
olawale <waleshogbaike@yahoo.com>
2022-09-15 11:25:04 -0700

    commit

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_square - prints a square using the # character
+ * @size: size of the square
+ *
+ * Return: void
+ */
+
+void print_square(int size)
+{
+	int length;
+	int width;
+
+	if (size > 0)
+	{
+		for (length = 0; length < size; length++)
+		{
+			for (width = 0; width < size; width++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
