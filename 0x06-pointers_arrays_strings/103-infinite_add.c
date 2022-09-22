f4fd76d128236ca54bf11645799eb8544e94e967
olawale <waleshogbaike@yahoo.com>
2022-09-22 07:12:29 -0700

    task 11

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -4,49 +4,44 @@
  * infinite_add - adds two numbers
  * @n1: first number
  * @n2: second number
- * @r: result
- * @size_r: result length
- * Return: sum
+ * @r: buffer for result
+ * @size_r: buffer size
+ *
+ * Return: address of r or 0
  */
-
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int i = 0, j = 0, k, 1 = 0, f, s, d = 0;
+	int i, j, k, l, m, n;
 
-	while (n1[i] != '\0')
-		i++;
-	while (n2[j] != '\0')
-		j++;
-	if (i > j)
-		1 = i;
-	else
-		i = j;
-	if (1 + 1 > size_r)
+	for (i = 0; n1[i]; i++)
+		;
+	for (j = 0; n2[j]; j++)
+		;
+	if (i > size_r || j > size_r)
 		return (0);
-	r[1] = '\0';
-	for (k = 1 - 1 ; k >= 0 ; k--)
+	m = 0;
+	for (i -= 1, j -= 1, k = 0; k < size_r - 1; i--, j--, k++)
 	{
-		i--;
-		j--;
+		n = m;
 		if (i >= 0)
-			f = n1[i] - '0';
-		else
-			f = 0;
+			n += n1[i] - '0';
 		if (j >= 0)
-			s = n2[j] - '0';
-		else
-			s = 0;
-		r[k] = (f + s + d) % 10 + '0';
-		d = (f + s + d) / 10;
+			n += n2[j] - '0';
+		if (i < 0 && j < 0 && n == 0)
+		{
+			break;
+		}
+		m = n / 10;
+		r[k] = n % 10 + '0';
 	}
-	if (d == 1)
+	r[k] = '\0';
+	if (i >= 0 || j >= 0 || m)
+		return (0);
+	for (k -= 1, l = 0; l < k; k--, l++)
 	{
-		r[1 + 1] = '\0';
-		if (1 + 2 > size_r)
-			return (0);
-		while (1-- >= 0)
-			r[1 + 1] = r[1];
-		r[0] = d + '0';
+		m = r[k];
+		r[k] = r[l];
+		r[l] = m;
 	}
 	return (r);
 }
