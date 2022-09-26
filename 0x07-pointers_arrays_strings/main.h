4a7f85ebb4cd56b722b2080b8b1af65894045e69
olawale <waleshogbaike@yahoo.com>
2022-09-26 07:47:11 -0700

    task 7

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,16 +1,19 @@
-#ifndef MAIN_H_
-#define MAIN_H_
+#ifndef MAIN_H
+#define MAIN_H
 
+#include <stdio.h>
+#include <stdlib.h>
 
-int _putchar(char);
-char *_memset(char *, char, unsigned int);
-char *_memcpy(char *, char *, unsigned int);
-char *_strchr(char *, char);
-unsigned int _strspn(char *, char *);
-char *_strpbrk(char *, char *);
-char *_strstr(char *, char *);
+int _putchar(char s);
+char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
+unsigned int _strspn(char *s, char *accept);
+char *_strpbrk(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
-void print_diagsums(int *, int);
-void set_string(char **, char *);
+void print_diagsums(int *a, int size);
+void set_string(char **s, char *to);
 
-#endif /* MAIN_H_*/
+
+#endif
