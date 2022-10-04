5e30c2c70be146646a6129892f01dc81e537cf15
olawale <waleshogbaike@yahoo.com>
2022-10-04 02:06:55 -0700

    task 6

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -55,13 +55,13 @@ char **strtow(char *str)
 			if (c)
 			{
 				end = i;
-				tmp = (char *) malloc(sizeof(char) * (c +1));
+				tmp = (char *) malloc(sizeof(char) * (c + 1));
 				if (tmp == NULL)
 					return (NULL);
 				while (start < end)
 					*tmp++ = str[start++];
 				*tmp = '\0';
-				matrix[k] = tmp -c;
+				matrix[k] = tmp - c;
 				k++;
 				c = 0;
 			}
