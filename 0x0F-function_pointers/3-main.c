cef04348f424dbb499162e6f5aae48313014780a
olawale <waleshogbaike@yahoo.com>
2022-10-12 13:56:27 -0700

    task update

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,32 +1,44 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "3-calc.h"
+#include <stdlib.h>
+#include <stdio.h>
 
 /**
- * main - performs simple operations
- * @argc: number of arguments passed
- * @argv: array of pointers to arguments
- * Return: always 0
+ * main - main entry point of the program
+ * @argc: count of command line arguments
+ * @argv: vector containing command line arguments
+ *
+ * Return: Always 0 (Successful)
  */
 int main(int argc, char *argv[])
 {
-	int a, b, c;
-	int (*f)(int, int);
+	int num1, num2, result;
+	char operator;
+	int (*f)(int, int); /*pointer to a function*/
 
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-	a = atoi(argv[1]);
-	b = atoi(argv[3]);
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+
 	f = get_op_func(argv[2]);
-	if (f == NULL)
+	if (f == NULL) /*Operator not found*/
 	{
 		printf("Error\n");
 		exit(99);
 	}
-	c = f(a, b);
-	printf("%d\n", c);
+
+	operator = *argv[2];
+	if ((operator == '/' || operator == '%') && num2 == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	result = f(num1, num2);
+	printf("%d\n", result);
 	return (0);
 }
